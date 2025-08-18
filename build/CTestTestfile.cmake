93ba1e869bdb64b94ffa908b7503e1d6c7a52b04
edureruem <wjdfyd0478@gmail.com>
2025-08-18 23:33:31 +0900

    influenceCurve Attr component Type child influenceCurveData, group ID

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/edurm/divkitBase/plug-ins/plug-ins/offsetCurveDeformer/offsetCurveDeformer
+# Build directory: C:/Users/edurm/divkitBase/plug-ins/plug-ins/offsetCurveDeformer/offsetCurveDeformer/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
